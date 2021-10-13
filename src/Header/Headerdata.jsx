0dcc9173d0302a0607567b731a46f8692c2568fb
JuliusHerrmann <herrmann.julius@gmx.de>
2021-10-13 04:39:59 +0200

    added last name to Gerrit

--- src/Header/Headerdata.jsx
+++ src/Header/Headerdata.jsx
@@ -7,7 +7,7 @@ class HeaderData extends React.Component{
       <div>
         <h1>Network Epidemic Playground</h1>
         <h2>Continuous-time stochastic simulation of epidemic spreading on human-to-human contact networks</h2>
-        <h3 id="credits">Simulation developed by <a href="https://mosi.uni-saarland.de/people/gerrit/" target="_blank" rel="noreferrer">Gerrit</a>, website developed by <a href="https://juliusherrmann.de" target="_blank" rel="noreferrer">Julius Herrmann</a></h3>
+        <h3 id="credits">Simulation developed by <a href="https://mosi.uni-saarland.de/people/gerrit/" target="_blank" rel="noreferrer">Gerrit Großmann</a>, website developed by <a href="https://juliusherrmann.de" target="_blank" rel="noreferrer">Julius Herrmann</a></h3>
       </div>
 		);
 	}
